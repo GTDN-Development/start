86baefa6fa46cd42d72e924f02a814e551ac3f3d
fanda-blazek <blazek127@gmail.com>
2025-10-20 19:00:43 +0200

    update number of socials by default to lower number

--- src/config/social-media-links.ts
+++ src/config/social-media-links.ts
@@ -1,23 +1,11 @@
 import type { NavigationLink } from "./nav-links";
-import {
-  FacebookIcon,
-  GitHubIcon,
-  InstagramIcon,
-  TwitterXIcon,
-  YoutubeIcon,
-  SpotifyIcon,
-} from "@/components/ui/icon-brand";
+import { InstagramIcon, TwitterXIcon, YoutubeIcon, GitHubIcon } from "@/components/ui/icon-brand";
 
 export type SocialMediaLink = NavigationLink & {
   icon: React.ComponentType<{ className?: string }>;
 };
 
 export const socialMediaLinks = {
-  facebook: {
-    name: "Facebook",
-    href: "https://www.facebook.com/user-name/",
-    icon: FacebookIcon,
-  },
   instagram: {
     name: "Instagram",
     href: "https://www.instagram.com/user-name/",
@@ -32,11 +20,6 @@ export const socialMediaLinks = {
     name: "YouTube",
     href: "https://www.youtube.com/@user-name",
     icon: YoutubeIcon,
-  },
-  spotify: {
-    name: "Spotify",
-    href: "https://open.spotify.com/user/user-name",
-    icon: SpotifyIcon,
   },
   github: {
     name: "GitHub",
